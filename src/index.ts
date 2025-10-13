130941afbc9de01ca31551bed03f700bd901c4a3
Ryan Smith <ryan.s.smith@bbc.co.uk>
2025-10-13 09:55:05 +0100

    type get by id endpoint

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,14 @@ app.get('/', (req: Request, res: Response<Pet[]>): void => {
     res.json(pets)
 })
 
-app.get('/:id', (req: Request<{id: string}>, res: Response<Pet | string>): void => {
+app.get('/:id', (req: Request<{id: string}>, res: Response<Pet | {message: string}>): void => {
     const { id } = req.params;
-    const foundPet = pets.find(pet => pet.id.toString() === id)
-    if (!foundPet) {
-        res.json("No pet with that ID!");
+    const pet: Pet | undefined = pets.find((pet: Pet): boolean => pet.id.toString() === id)
+    if (!pet) {
+        res.json({message: "No pet with that ID!"});
         return
     }
-    res.status(200).json(foundPet)
+    res.status(200).json(pet)
 })
 
 app.use((req: Request, res: Response<{message: string}>): void => {
