b3774acee381a739aa57bd92706a2bb791a02d61
Ryan Smith <ryan.s.smith@bbc.co.uk>
2025-10-17 12:22:18 +0100

    feat: seperate pet logic into controller

--- src/routes/pet.routes.ts
+++ src/routes/pet.routes.ts
@@ -1,52 +1,12 @@
 import express from 'express';
-import { pets } from "../data/pets.js";
-import type { Router, Request, Response} from 'express'
-import type { Pet } from "../data/pets.js";
+import type { Router } from 'express'
+
+import { getPets, getPetById } from '../controllers/pets.controller.js';
 
 const petRouter: Router = express.Router();
 
-type PetQueryParams = {
-    species?: string,
-    adopted?: 'true' | 'false',
-    minAge?: string,
-    maxAge?: string
-};
+petRouter.get('/', getPets)
 
-petRouter.get('/', (
-    req: Request<{}, unknown, {}, PetQueryParams>,
-    res: Response<Pet[]>
-): void => {
-    const { species, adopted, minAge, maxAge } = req.query;
-    
-    let filteredPets: Pet[] = pets;
-    
-    if (species) {
-       filteredPets = filteredPets.filter((pet: Pet): boolean => pet.species.toLowerCase() === species.toLowerCase());
-    };
-
-    if (adopted) {
-        const boolAdopted = (adopted === "true");
-        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.adopted === boolAdopted);
-    };
-
-    if (minAge) {
-        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.age >= Number(minAge));
-    }; 
-    if (maxAge) {
-        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.age <= Number(maxAge));
-    };
-
-    res.status(200).json(filteredPets);
-})
-
-petRouter.get('/:id', (req: Request<{id: string}>, res: Response<Pet | {message: string}>): void => {
-    const { id } = req.params;
-    const pet: Pet | undefined = pets.find((pet: Pet): boolean => pet.id.toString() === id);
-    if (!pet) {
-        res.json({message: "No pet with that ID!"});
-        return
-    }
-    res.status(200).json(pet)
-});
+petRouter.get('/:id', getPetById);
 
 export default petRouter;
