0b82b72d725e70d9d2b2a4bcfc61ce00a6f50e37
Ryan Smith <ryan.s.smith@bbc.co.uk>
2025-10-17 12:01:41 +0100

    feat: filter by minAge and maxAge

--- src/index.ts
+++ src/index.ts
@@ -12,41 +12,52 @@ app.use(cors());
 
 type PetQueryParams = {
     species?: string,
-    adopted?: string
-}
+    adopted?: 'true' | 'false',
+    minAge?: string,
+    maxAge?: string
+};
 
 app.get('/', (
     req: Request<{}, unknown, {}, PetQueryParams>,
     res: Response<Pet[]>
 ): void => {
-    const { species, adopted } = req.query;
+    const { species, adopted, minAge, maxAge } = req.query;
     
     let filteredPets: Pet[] = pets;
     
     if (species) {
        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.species.toLowerCase() === species.toLowerCase());
-    }
+    };
+
     if (adopted) {
-        const boolAdopted = (adopted === "true")
-        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.adopted === boolAdopted)
-    }
+        const boolAdopted = (adopted === "true");
+        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.adopted === boolAdopted);
+    };
+
+    if (minAge) {
+        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.age >= Number(minAge));
+    }; 
+    if (maxAge) {
+        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.age <= Number(maxAge));
+    };
+
     res.status(200).json(filteredPets);
 })
 
 app.get('/:id', (req: Request<{id: string}>, res: Response<Pet | {message: string}>): void => {
     const { id } = req.params;
-    const pet: Pet | undefined = pets.find((pet: Pet): boolean => pet.id.toString() === id)
+    const pet: Pet | undefined = pets.find((pet: Pet): boolean => pet.id.toString() === id);
     if (!pet) {
         res.json({message: "No pet with that ID!"});
         return
     }
     res.status(200).json(pet)
-})
+});
 
 app.use((req: Request, res: Response<{message: string}>): void => {
-  res.status(404).json({message: "Endpoint not found"})
-})
+  res.status(404).json({message: "Endpoint not found"});
+});
 
 app.listen(PORT, (): void => {
-    console.log(`Listening on port: ${PORT}`)
-})
+    console.log(`Listening on port: ${PORT}`);
+});
