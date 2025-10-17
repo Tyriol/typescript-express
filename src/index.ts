6425809c59fbdad386e0e7b8deec226b948f4a8c
Ryan Smith <ryan.s.smith@bbc.co.uk>
2025-10-17 12:09:56 +0100

    feat: seperate pets into its own route

--- src/index.ts
+++ src/index.ts
@@ -2,57 +2,14 @@ import express from "express";
 import type { Express, Request, Response } from "express";
 import cors from "cors"
 
-import { pets } from "./data/pets.js";
-import { Pet } from "./data/pets.js";
+import petRouter from "./routes/pet.routes.js";
 
 const PORT = 8000;
 const app: Express = express();
 
 app.use(cors());
 
-type PetQueryParams = {
-    species?: string,
-    adopted?: 'true' | 'false',
-    minAge?: string,
-    maxAge?: string
-};
-
-app.get('/', (
-    req: Request<{}, unknown, {}, PetQueryParams>,
-    res: Response<Pet[]>
-): void => {
-    const { species, adopted, minAge, maxAge } = req.query;
-    
-    let filteredPets: Pet[] = pets;
-    
-    if (species) {
-       filteredPets = filteredPets.filter((pet: Pet): boolean => pet.species.toLowerCase() === species.toLowerCase());
-    };
-
-    if (adopted) {
-        const boolAdopted = (adopted === "true");
-        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.adopted === boolAdopted);
-    };
-
-    if (minAge) {
-        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.age >= Number(minAge));
-    }; 
-    if (maxAge) {
-        filteredPets = filteredPets.filter((pet: Pet): boolean => pet.age <= Number(maxAge));
-    };
-
-    res.status(200).json(filteredPets);
-})
-
-app.get('/:id', (req: Request<{id: string}>, res: Response<Pet | {message: string}>): void => {
-    const { id } = req.params;
-    const pet: Pet | undefined = pets.find((pet: Pet): boolean => pet.id.toString() === id);
-    if (!pet) {
-        res.json({message: "No pet with that ID!"});
-        return
-    }
-    res.status(200).json(pet)
-});
+app.use('/pets', petRouter);
 
 app.use((req: Request, res: Response<{message: string}>): void => {
   res.status(404).json({message: "Endpoint not found"});
